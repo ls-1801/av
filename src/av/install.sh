bcf524f5225dc23a77e331dbb4d1c178a662749b
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-08-29 20:32:54 +0200

    adds av feature

---
+++ src/av/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'av'"
+
+AV_VERSION="0.0.42"
+wget https://github.com/aviator-co/av/releases/download/v${AV_VERSION}/av_${AV_VERSION}_linux_$(dpkg --print-architecture).deb
+apt install ./av_${AV_VERSION}_linux_$(dpkg --print-architecture).deb
+
+av --version
